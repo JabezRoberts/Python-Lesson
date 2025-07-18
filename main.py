d0b9307a840679a302da2cfcc309d86702f199e4
Jabez Roberts <info@jabezroberts.com>
2025-07-17 21:35:28 -0500

    List comprehension for different Python file types
    List comprehension for lists, strings, and  dictionaries, combined with the pandas library, and using both to create a NATO phonetic alphabet

--- main.py
+++ main.py
@@ -1,123 +1,35 @@
-from turtle import Screen
-from mysnake import Snake
-import time
-from food import Food
-from scoreboard import Scoreboard
+import pandas
 
+# student_dict = {
+#     "student": ["Angela", "James", "Lily"], 
+#     "score": [56, 76, 98]
+# }
 
-screen =  Screen()
-screen.setup(width=600, height=600)
-screen.bgcolor("black")
-screen.title("My Snake Game")
-screen.tracer(0)
+# #Looping through dictionaries:
+# for (key, value) in student_dict.items():
+#     #Access key and value
+#     pass
 
+# import pandas
+# student_data_frame = pandas.DataFrame(student_dict)
 
-snake = Snake()
-food = Food()
-scoreboard = Scoreboard()
+# #Loop through rows of a data frame
+# for (index, row) in student_data_frame.iterrows():
+#     #Access index and row
+#     #Access row.student or row.score
+#     pass
 
-screen.listen()
+# # Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
 
-screen.onkey(snake.up, "Up") # tie buttons to these functions we create
-screen.onkey(snake.down, "Down")
-screen.onkey(snake.left, "Left")
-screen.onkey(snake.right, "Right")
+name_data = pandas.read_csv("C:/Users/Zeilhan Co/Desktop/Study/100 Days of Code Python/Code/Day 26 - Intermediate List Comprehension & NATO Alphabet/NATO Alphabet Project/NATO-alphabet-start/nato_phonetic_alphabet.csv")
+print(name_data.to_dict())
+print("lol")
+#TODO 1. Create a dictionary in this format:
+phonetic_dictionary = {row.letter:row.code for (index, row) in name_data.iterrows()}
+print(phonetic_dictionary)
 
-
-# Create a snake body ==> 3 squares (turtles) all ligned up behind each other. Each square is 20x20 pixels.
-# The first square is at (0, 0), the second at (-20, 0), and the third at (-40, 0). 
-
-# segment_1 = Turtle("square")
-# segment_1.color("white")
-
-# segment_2 = Turtle("square")
-# segment_2.color("white")
-# segment_2.goto(-20, 0)
-
-# segment_3 = Turtle("square")
-# segment_3.color("white")
-# segment_3.goto(-40, 0)
-# Written in a more efficient way using a loop
-
-
-# for position in starting_positions:
-#     segment = Turtle("square")
-#     segment.color("white")
-#     # segment.penup()  # Prevents the turtle from drawing lines
-#     segment.goto(position)
-
-# Now to move the snake, we can create a function that moves each segment forward by 20 pixels.
-#Create and organize the snake segments into a list for easy management.
-
-snake_segments = []
-segments = []
-
-
-game_is_on = True
-while game_is_on:
-    screen.update()
-    time.sleep(0.1) # Adds a 1 second sleep/pause/delay after all segments have movement
-    
-    snake.move()
-    
-    # detect collision with food using distance method from Turtle class
-    if snake.head.distance(food) < 15:
-        food.refresh()
-        snake.extend()
-        scoreboard.increase_score()
-    
-    # Detect collision with wall
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
-    
-    
-    # Detect collision with tail
-    # if head collides with any segment in tail trigger game over sequence
-    # for segment in snake.segments:
-    #     if segment == snake.head:
-    #         pass
-    #     elif snake.head.distance(segment) < 10:
-    #         game_is_on = False
-    #         scoreboard.game_over()
-    # rewrite the above in fewer lines
-    for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
-    
-# How to control the snake using the direction keys
-
-
-screen.exitonclick()# Uncomment the following lines to run the snake game with the initial setup.
-
-
-
-# from turtle import Screen
-# from snake import Snake
-# import time
-
-# screen = Screen()
-# screen.setup(width=600, height=600)
-# screen.bgcolor("black")
-# screen.title("My Snake Game")
-# screen.tracer(0)
-
-# snake = Snake()
-# food = Food()
-
-# screen.listen()
-# screen.onkey(snake.up, "Up")
-# screen.onkey(snake.down, "Down")
-# screen.onkey(snake.left, "Left")
-# screen.onkey(snake.right, "Right")
-
-# game_is_on = True
-# while game_is_on:
-#     screen.update()
-#     time.sleep(0.1)
-
-#     snake.move()
-
-
-# screen.exitonclick()
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+user_input = input("Enter a word: ").upper()
+output = [phonetic_dictionary[letter] for letter in user_input]
+print(output)
--- tutorial.py
+++ tutorial.py
@@ -1,18 +1,154 @@
-from turtle import Turtle, Screen
+import random
+import pandas
 
-wartortle = Turtle() # Create a Turtle instance
-screen = Screen() # Create a Screen object or instance
+numbers = [1, 2, 3]
+new_list = []
+for n in numbers:
+    add_1 = n + 1
+    new_list.append(add_1)
 
-def move_forward():
-    wartortle.forward(10) # Move the turtle forward by 10 units
+# Can be written as
+"""new_list = [new_item for item in list]"""
+numbers = [1, 2, 3]
+new_list = [n + 1 for n in numbers]
 
-screen.listen() # Set the screen to listen for events
-# Use an event listener to connect our screen to our keyboard input
+# It also works with Strings, Tuples, Dictionaries
+name = "Angela"
+letters_list = [letter for letter in name] # split off your string to individual letters and added them to a brand new list
 
-screen.onkey(key="space", fun=move_forward) # When the Space key is pressed, call move_forward. When you pass a function as an argument, you don't include the parentheses.
-# Parentheses would call the function immediately, which is not what we want here. Instead, we want to pass the function itself as a reference to be called later when the event occurs.
+# Create a range(1,5) and create a list where each number is doubled
+doubled_nums = [num * 2 for num in range(1,5)]
 
-# A Higher Order Function is a function that takes another function as an argument or returns a function as its result. In this case, `screen.onkey` is a higher order function because it takes `move_forward` as an argument.
+# Conditional list comprehension
+"""new_list = [new_item for item in list if test]"""
+names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"] # Now return a list of all the names with less than 5 letters
+short_names = [name for name in names if len(name) < 5]
 
-screen.exitonclick() # Wait for a click on the screen to exit the program
-# This allows us to close the turtle graphics window when we're done.
+# Convert all names with 6 letters or more to Uppercase
+uppercase_names = [name.upper() for name in names if len(name) > 5]
+
+
+
+
+# Dictionary Comprehension
+"""
+new_dict = {new_key:new_value for item in list}
+new_dict = {new_key:new_value for (key,value) in dict.items()}
+new_dict = {new_key:new_value for (key,value) in dict.items() if test}
+"""
+names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+# Now create a dictionary where you generate a random score for each of them
+student_scores = {student:random.randint(1,100) for student in names}
+
+
+# Now loop through a dictionary and return a new dictionary if some condition is satisfied.
+passed_students = {student:score for (student,score) in student_scores.items() if score > 80}
+
+
+# Using list comprehension with dictionary and pandas dataframe
+student_dict = {
+    "Student": ["Angela", "James", "John"],
+    "Grades": [56,90,76]
+}
+
+# Looping through dictionaries:
+for (key, value) in student_dict.items():
+    print(value)
+student_data_frames = pandas.DataFrame(student_dict)
+print(student_data_frames)
+
+# Loop through a data frame
+for (key, value) in student_data_frames.items():
+    print(key) #gives titles of columns
+    print(value) #give data of columns but loops through names of columns then data in each column which makes the data not useful
+
+# to loop through the rows of the data frame instead use iterrows
+for (index, row) in student_data_frames.iterrows():
+    print(index)
+    print(row) # check the output of this --> it prints first row with student and score then second row then third
+    print(row.student) # First prints the entire data frame then each of the student in that data frame
+    print(row.score) # First prints the entire data frame then each of the scores in that data frame
+    
+    if row.student == "Angela":
+        print(row.score) # Would print Angela and her score
+
+
+# Dict list comprehension
+{new_key:new_value for (key,value) in dict.items()}
+
+# Use instead to loop through a data frame
+{new_key:new_value for (index, row) in df.iterrows()}
+
+
+
+
+# 💪 This exercise is HARD 💪 
+
+# Take a look inside file1.txt and file2.txt. They each contain a bunch of numbers, each number on a new line. 
+
+# You are going to create a list called result which contains the numbers that are common in both files. 
+
+# e.g. if file1.txt contained: 
+
+# 1 
+
+# 2 
+
+# 3
+
+# and file2.txt contained: 
+
+# 2
+
+# 3
+
+# 4
+
+# result = [2, 3]
+
+
+
+# IMPORTANT:  The output should be a list of integers and not strings!
+
+# Try to use List Comprehension instead of a Loop. 
+
+with open("file1.txt") as file1:
+  list1 = file1.readlines()
+    
+with open("file2.txt") as file2:
+  list2 = file2.readlines()
+    
+result = [int(num) for num in list1 if num in list2]
+ 
+print(result)
+
+
+# You are going to use Dictionary Comprehension to create a dictionary called result that takes each word in the given sentence and calculates the number of letters in each word.   
+
+# Try Googling to find out how to convert a sentence into a list of words.  *
+
+# *Do NOT** Create a dictionary directly.
+
+# Try to use Dictionary Comprehension instead of a Loop. 
+
+# To keep this exercise simple, count any punctuation following a word with no whitespace as part of the word. Note that "Swallow?" therefore has a length of 8.
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+result = {word: len(word) for word in sentence.split()}
+print(result)
+
+
+
+# You are going to use Dictionary Comprehension to create a dictionary called weather_f that takes each temperature in degrees Celsius and converts it into degrees Fahrenheit. 
+
+# To convert temp_c into temp_f use this formula: 
+
+(temp_c * 9/5) + 32 = temp_f
+
+
+
+weather_c = {"Monday": 12, "Tuesday": 14, "Wednesday": 15, "Thursday": 14, "Friday": 21, "Saturday": 22, "Sunday": 24}
+ 
+weather_f = {day:temp * 9/5 + 32 for (day, temp) in weather_c.items()}
+ 
+print(weather_f)
+
